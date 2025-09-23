28976712d6030bc5930a72e80503d8528ccb3840
single_ptilopsis <ptilopsis_program@foxmail.com>
2025-09-23 17:50:44 +0800

    feat: 添加GitHub链接图标

--- src/App.vue
+++ src/App.vue
@@ -85,21 +85,36 @@ function handleSwitchToken(token: string) {
         </el-table>
     </el-dialog>
     <div class="header">
-        <el-icon v-if="isDashboardRoute" style="margin-right: 10px;" @click="ifShowSidebar = !ifShowSidebar">
-            <i-ep-fold v-if="ifShowSidebar" />
-            <i-ep-expand v-else />
-        </el-icon>
-        WTM Console
-        <div style="display: flex; flex-grow: 1; justify-content: flex-end; padding-right: 40px;">
-            <div style="cursor: pointer; display: flex; align-items: center; margin-right: 20px;"
+        <div style="display: flex; align-items: center;">
+            <el-icon v-if="isDashboardRoute" style="margin-right: 10px;" @click="ifShowSidebar = !ifShowSidebar">
+                <i-ep-fold v-if="ifShowSidebar" />
+                <i-ep-expand v-else />
+            </el-icon>
+            <div>
+                WTM Console
+            </div>
+        </div>
+        <div style="display: flex; align-items: center;">
+            <div style="cursor: pointer; display: flex; align-items: center; margin-right: 10px;"
                 @click="ifShowSwitch = true">
                 <el-icon style="margin-right: 5px;"><i-ep-Switch /></el-icon>
                 切换
             </div>
-            <div v-show="isDashboardRoute" style="cursor: pointer; display: flex; align-items: center;"
+            <div v-show="isDashboardRoute" style="cursor: pointer; display: flex; align-items: center"
                 @click="logout(false)">
                 <el-icon style="margin-right: 5px;"><i-ep-SwitchButton /></el-icon>
                 登出
+            </div>
+            <el-divider direction="vertical" style="margin: 0 10px;" />
+            <div style="height: 20px; width: 20px; display: flex; align-items: center; margin-right: 40px;">
+                <a href="https://github.com/TiebaMeow/WebTiebaManager" target="_blank"
+                    style="display: flex; align-items: center;">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96" style="height: 20px">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                            fill="#24292f" />
+                    </svg>
+                </a>
             </div>
         </div>
     </div>
@@ -152,5 +167,6 @@ body.el-popup-parent--hidden {
     padding: 0 20px;
     font-size: 20px;
     border-bottom: 1px solid rgb(217, 217, 217);
+    justify-content: space-between;
 }
 </style>
